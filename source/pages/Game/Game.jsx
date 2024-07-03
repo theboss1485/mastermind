900ac619bf0ee71397924085e1cfba76c289f148
DOOM Guy 1485 <theboss1485@gmail.com>
2024-07-03 15:57:03 -0400

    Completed Game

--- source/pages/Game/Game.jsx
+++ source/pages/Game/Game.jsx
@@ -2,14 +2,20 @@
 
 import Instructions from '../Instructions/Instructions';
 import './Game.css'
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import {Link} from 'react-router-dom';
 import GameBoard from '../../components/GameBoard/GameBoardAndColorPicker';
 
 export default function Game(props){
 
+    const childRef = useRef();
+
     const [displayInstructions, setDisplayInstructions] = useState(false);
     const [gameInProgress, setGameInProgress] = useState(false);
+    const [gameComplete, setGameComplete] = useState(false);
+    const [colorCode, setColorCode] = useState([]);
+    const [blackWhitePegs, setBlackWhitePegs] = useState(null);
+    const [forceGameBoardUpdate, setForceGameBoardUpdate] = useState(false)
 
     
 
@@ -24,17 +30,116 @@ export default function Game(props){
         }
     }
 
-    const handleGuess = () => {
+    const handleGuess = async (guess, guessCounter) => {
 
-        console.log("guess made!!")
+        if(guess.includes('unset')){
+
+            await childRef.current.flashCircles('darkred', guess);
+
+        } else {
+
+            let colorCodeCopy = [...colorCode]
+            let guessCopy = [...guess]
+    
+            let correctColorCorrectPosition = 0;
+            let correctColorIncorrectPosition = 0;
+    
+            for (let loopCounter1 = 0; loopCounter1 < colorCodeCopy.length; loopCounter1++){
+    
+                if(guessCopy[loopCounter1] === colorCodeCopy[loopCounter1]){
+    
+                    correctColorCorrectPosition++;
+                    guessCopy[loopCounter1] = null;
+                    colorCodeCopy[loopCounter1] = null;
+    
+                }
+            }
+    
+            for (let loopCounter2 = 0; loopCounter2 < colorCodeCopy.length; loopCounter2++){
+    
+                if(guessCopy[loopCounter2] !== null){
+
+                    let guessIndex = colorCodeCopy.indexOf(guessCopy[loopCounter2])
+    
+                    if(guessIndex !== -1){
+    
+                        correctColorIncorrectPosition++
+                        guessCopy[loopCounter2] = null;
+                        colorCodeCopy[guessIndex] = null;
+        
+                    }
+                }
+            }
+    
+            setBlackWhitePegs({correctColorCorrectPosition: correctColorCorrectPosition, correctColorIncorrectPosition: correctColorIncorrectPosition})
+
+            if(correctColorCorrectPosition === 4){
+
+                await childRef.current.flashCircles('green', null, true);
+                childRef.current.drawActualCodeCircles(null, colorCode);
+
+            } else if (guessCounter === 11 && correctColorCorrectPosition !== 4){
+
+                await childRef.current.flashCircles('darkred', null, true);
+                childRef.current.drawActualCodeCircles(null, colorCode);
+            }
+        }
+    }
+
+    const handleGameComplete = async () => {
+
+        setGameComplete(true);
+        setGameInProgress(false);
     }
 
     const playGame = () => {
 
-        setGameInProgress(true)
+        generateColorCode();
+        setGameInProgress(true);
+        if(gameComplete === true){
+            setForceGameBoardUpdate(!forceGameBoardUpdate)
+        }
+        setGameComplete(false);
     }
 
+    const quit = () => {
 
+        setGameComplete(false);
+    }
+
+    const generateColorCode = () => {
+
+         let generatedColorCode = [];
+
+        for (let generatedColorCodeInteger = 0; generatedColorCodeInteger < 4; generatedColorCodeInteger++){
+
+            let randomInteger = Math.floor(Math.random() * 6)
+
+            switch(randomInteger){
+
+                case 0:
+                    generatedColorCode.push('blue');
+                    break;
+                case 1:
+                    generatedColorCode.push('green');
+                    break;
+                case 2:
+                    generatedColorCode.push('red');
+                    break;
+                case 3:
+                    generatedColorCode.push('cyan');
+                    break;
+                case 4:
+                    generatedColorCode.push('magenta');
+                    break;
+                case 5:
+                    generatedColorCode.push('yellow');
+                    break;
+            }
+        }
+
+        setColorCode(generatedColorCode)
+    }
 
     return (
         
@@ -58,7 +163,18 @@ export default function Game(props){
 
             )}
             {displayInstructions && (<Instructions onBackButtonClick={toggleInstructions}/>)} 
-            {gameInProgress && (<GameBoard onGuess={handleGuess}/>)} 
+            {(gameInProgress || gameComplete) && (<GameBoard onGuess={handleGuess} onGameComplete={handleGameComplete} forceUpdate={forceGameBoardUpdate} calculatedGuessData={blackWhitePegs} ref={childRef} />)}
+            {gameComplete && (
+                <>
+                    <div className='fit-content mx-auto my-2'>
+                        <button onClick={playGame}>Play Again</button>
+                    </div>
+                    <div className='fit-content mx-auto my-2'>
+                        <button onClick={quit}>Quit</button>
+                    </div>
+                </>
+                
+            )}
         </div>
     )
 }
